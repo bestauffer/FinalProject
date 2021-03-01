800c194d182c9bc26193713c68b08f5e91152fa8
bestauffer <77029920+bestauffer@users.noreply.github.com>
2021-03-01 12:24:25 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// start by creating data so we don't have to type it in each time
+let movieArray = [];
+let selectedGenre = "not selected";
+
+document.addEventListener("DOMContentLoaded", function () {
+
+// add button events ************************************************************************
+  
+// end of add button events ************************************************************************
+
+  
+  
+// page before show code *************************************************************************
+ 
+// end of page before show code *************************************************************************
+
+});  
+// end of wait until document has loaded event  *************************************************************************
+
+function createList()
+{
+   
+};
+  
