513a7f42ea3ccadf726f85f2d2fab7f8b9001e17
bestauffer <77029920+bestauffer@users.noreply.github.com>
2021-03-03 22:44:23 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 // start by creating data so we don't have to type it in each time
 let beerArray = [];
-
+let counter = 0;
 // define a constructor to create movie objects
-let beerObject = function (pName) {
-    this.ID;
+let BeerObject = function (pName) {
+    
     this.Name = pName;
     this.Location;
     this.Price;
     this.Description;  
-    this.rating;    
+    this.Rating;    
 }
 
 let sessionID;
 function Data(ID){
     sessionID = ID;
+    document.getElementById("Location").innerHTML = beerArray[ID].Location;
+    document.getElementById("Price").innerHTML = beerArray[ID].Price;
+    document.getElementById("Description").innerHTML = beerArray[ID].Description;
+    document.getElementById("Ratings").innerHTML = beerArray[ID].Rating;
+    document.getElementById("Name").innerHTML = beerArray[ID].Name;
+
+
 }
+
+beerArray.push(new BeerObject("Bud Light"));
+beerArray.push(new BeerObject("Rainier"));
+beerArray.push(new BeerObject("Irish Death"));
+
+beerArray[0].Location = "Albertsons";
+beerArray[0].Price = 10;
+beerArray[0].Description = "Bud light is very refreshing on a hot day";
+beerArray[0].Rating = 3;
+
+beerArray[1].Location = "Safeway";
+beerArray[1].Price = 8;
+beerArray[1].Description = "Rainier is a classic beer that tastes amazing any day";
+beerArray[1].Rating = 4;
+
+beerArray[2].Location = "Norm's";
+beerArray[2].Price = 12;
+beerArray[2].Description = "Irish Death has a chocolate flavor that is to die for!";
+beerArray[2].Rating = 5;
