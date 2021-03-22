e146adc5f133e3fc4aa03d0236dfdfcb20b702ea
bestauffer <77029920+bestauffer@users.noreply.github.com>
2021-03-22 16:06:01 -0700

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -436,8 +436,8 @@ function deleteBeer(deletedBeerID){
         method: 'DELETE'
     })
     .then(function (theResponsePromise) {    // the .json sets up 2nd promise
-        console.log("beer ID " + deletedBeerID + "has been deleted");
-        alert("beer ID " + deletedBeerID + "has been deleted"); 
+        
+        alert("beer ID " + deletedBeerID + "has been deleted"), 
         document.location.href = "#" 
     })
     // the client console log will write out the message I added to the Repsonse on the server
